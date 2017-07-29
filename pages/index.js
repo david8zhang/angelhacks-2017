6a7388fd62edf25f8c1bc555c4a5b789eb0ec75a
Edward Zhang <edwardz123@gmail.com>
2017-07-29 15:46:51 -0700

    Merge master fo real

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-export { default as HomePage } from './HomePage';
 export { default as ChatRoomsPage } from './ChatRoomsPage';
-=======
 export { default as ChatPage } from './ChatPage';
->>>>>>> f74a31b0d95cb3e7fac566cfd22a795b8c1daa35
