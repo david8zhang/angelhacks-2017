2a369107f9d7555a0eaddc60ccacad1ac1373f5b
Audrey Thompson <audreydnthompson@gmail.com>
2017-07-30 00:16:29 -0700

    Added achivement modals

--- pages/ProfilePage/index.js
+++ pages/ProfilePage/index.js
@@ -1,49 +1,140 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { Alert, Modal, View, Text, Image, TouchableOpacity, Button } from 'react-native';
 import { BottomBar } from '../../components';
 import { Actions } from 'react-native-router-flux';
 
 class ProfilePage extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			modalVisible: false,
+			modalText: 'Modal Text',
+			modalImage: null
+		};
+	}
+
+	_onPressButton(symbolName) {
+		const { modalImageStyle } = styles;
+		let text = 'Modal Text';
+		let image = null;
+		switch(symbolName) {
+			case 'singlePin':
+				text = 'Dropped first pin';
+				image = (
+					<Image
+		    			source={require('../../assets/icons/singlePin.png')}
+		    			style={modalImageStyle}
+		    		/>
+				);
+				break;
+			case 'singleGroup':
+				text = 'Joined first group';
+				image = (
+					<Image
+		    			source={require('../../assets/icons/singleGroup.png')}
+		    			style={modalImageStyle}
+		    		/>
+				);
+				break;
+			case 'singleTrash':
+				text = 'Completed first cleanup';
+				image = (
+					<Image
+		    			source={require('../../assets/icons/singleTrash.png')}
+		    			style={modalImageStyle}
+		    		/>
+				);
+				break;
+			case 'multiPin':
+				text = 'Dropped 10 pins';
+				image = (
+					<Image
+		    			source={require('../../assets/icons/multiPin.png')}
+		    			style={modalImageStyle}
+		    		/>
+				);
+				break;
+			case 'multiGroup':
+				text = 'Joined 10 groups';
+				image = (
+					<Image
+		    			source={require('../../assets/icons/multiGroup.png')}
+		    			style={modalImageStyle}
+		    		/>
+				);
+				break;
+			case 'multiTrash':
+				text = 'Completed 10 cleanups';
+				image = (
+					<Image
+		    			source={require('../../assets/icons/multiTrash.png')}
+		    			style={modalImageStyle}
+		    		/>
+				);
+				break;
+			default:
+				text = 'Modal Text';
+		}
+		this.setState({modalVisible: true, modalText: text, modalImage: image});
+	}
+
+	closeModal() {
+		this.setState({modalVisible: false});
+	}
+
 	renderAchievements() {
 		const { stats, bubble, bubblePic } = styles;
 		return (
 			<View style={stats}>
-				<View style={bubble}>
-					<Image 
-						source={require('../../assets/icons/singlePin.png')}
-						style={bubblePic}
-					/>
-				</View>
-				<View style={bubble}>
-					<Image
-						source={require('../../assets/icons/singleGroup.png')}
-						style={bubblePic}
-					/>
-				</View>
-				<View style={bubble}>
-					<Image
-						source={require('../../assets/icons/singleTrash.png')}
-						style={bubblePic}
-					/>
-				</View>
-				<View style={bubble}>
-					<Image
-						source={require('../../assets/icons/multiPin.png')}
-						style={bubblePic}
-					/>
-				</View>
-				<View style={bubble}>
-					<Image
-						source={require('../../assets/icons/multiGroup.png')}
-						style={bubblePic}
-					/>
-				</View>
-				<View style={bubble}>
-					<Image
-						source={require('../../assets/icons/multiTrash.png')}
-						style={bubblePic}
-					/>
-				</View>
+				<TouchableOpacity onPress={() => {this._onPressButton('singlePin')}} underlayColor='#59D988'>
+					<View style={bubble}>
+						<Image 
+							source={require('../../assets/icons/singlePin.png')}
+							style={bubblePic}
+						/>
+					</View>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => {this._onPressButton('singleGroup')}} underlayColor='#59D988'>
+					<View style={bubble}>
+						<Image
+							source={require('../../assets/icons/singleGroup.png')}
+							style={bubblePic}
+						/>
+					</View>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => {this._onPressButton('singleTrash')}} underlayColor='#59D988'>
+					<View style={bubble}>
+						<Image
+							source={require('../../assets/icons/singleTrash.png')}
+							style={bubblePic}
+						/>
+					</View>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => {this._onPressButton('multiPin')}} underlayColor='#59D988'>
+					<View style={bubble}>
+						<Image
+							source={require('../../assets/icons/multiPin.png')}
+							style={bubblePic}
+						/>
+					</View>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => {this._onPressButton('multiGroup')}} underlayColor='#59D988'>
+					<View style={bubble}>
+						<Image
+							source={require('../../assets/icons/multiGroup.png')}
+							style={bubblePic}
+						/>
+					</View>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => {this._onPressButton('multiTrash')}} underlayColor='#59D988'>
+					<View style={bubble}>
+						<Image
+							source={require('../../assets/icons/multiTrash.png')}
+							style={bubblePic}
+						/>
+					</View>
+				</TouchableOpacity>
 			</View>
 		);
 	}
@@ -72,7 +163,11 @@ class ProfilePage extends Component {
 			achievementsText,
 			stats,  
 			divider,
-			bottomBar
+			bottomBar,
+			modalStyle,
+			modalTextStyle,
+			closeButton,
+			modalImageContainer
 		} = styles;
 		return (
 			<View style={outerFlex}>
@@ -92,6 +187,24 @@ class ProfilePage extends Component {
 						</Text>
 					</View>
 					<View style={divider}></View>
+					<Modal
+						animationType={"slide"}
+				        transparent={true}
+				        visible={this.state.modalVisible}
+				    >
+				    	<View style={modalStyle}>
+				    		<View style={modalImageContainer}>
+				    			{this.state.modalImage}
+				    		</View>
+				    		<Text style={modalTextStyle}>{this.state.modalText}</Text>
+				    		<Button
+				    			title={"Close"}
+				    			onPress={() => {this.closeModal()}}
+				    			style={closeButton}
+				    			color={'#59D988'}
+				    		/>
+				    	</View>
+				    </Modal>
 					<View style={achievementsPos}>
 						<Text style={achievementsText}>
 							Achievements
@@ -158,6 +271,49 @@ const styles = {
 		width: 50,
 		height: 50
 	},
+	modalStyle: {
+		width: 300,
+		height: 300,
+		backgroundColor: 'white',
+		alignSelf: 'center',
+		marginTop: 170,
+		borderColor: '#59D988',
+		borderRadius: 2,
+		borderWidth: 1
+	},
+	modalTextStyle: {
+		flex: 4,
+		fontSize: 30,
+		alignSelf: 'center',
+		textAlign: 'center',
+		flexDirection: 'column',
+		marginLeft: 5,
+		marginRight: 5
+	},
+	modalImageStyle: {
+		flex: 4,
+		flexDirection: 'column',
+		width: 60,
+		height: 60,
+		borderRadius: 30,
+		alignSelf: 'center',
+		marginTop: 20,
+		marginBottom: 20
+	},
+	closeButton: {
+		flex: 1,
+		flexDirection: 'column'
+	},
+	modalImageContainer: {
+		backgroundColor: '#acecc3',
+		width: 100,
+		height: 100,
+		borderRadius: 50,
+		flexDirection: 'column',
+		alignSelf: 'center',
+		marginTop: 70,
+		marginBottom: 5
+	}
 };
 
 export default ProfilePage;
